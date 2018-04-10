ba3aa93ae8e4e52ec090ba604cd418ae9bf1c7c6
Shourya <arorashourya94@gmail.com>
2018-04-10 10:12:56 +0530

    sankdone

--- titanicpython.py
+++ titanicpython.py
@@ -49,7 +49,7 @@ pd.read_excel('C:\\Users\\Defiance\\.spyder-py3\\Book1.xlsx')
 my_solution.to_excel("Book1.xlsx")
 
 #this is a comment
-#this is in sank
+#this is in sankdone
 
 print(my_solution)
 print("Press Enter to exit")
